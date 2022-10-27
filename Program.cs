6c2745dacb1766f8f049956741c6a89075e5ab69
feder <federicaelia24@gmail.com>
2022-10-27 10:24:33 +0200

    Finite richieste principali

--- Program.cs
+++ Program.cs
@@ -1,4 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
-string[] cibiPreferiti = { "Pasta", "Patate", "Broccoli" };
+string[] cibiPreferiti = { "Pasta", "Patate", "Broccoli", "Polpette", "Pasta al pesto" };
+
+//lunghezza array
+Console.WriteLine(cibiPreferiti.Length);
+
+//stampare l’intero array in ordine indicando la posizione in classifica
+for (int i = 0; i < cibiPreferiti.Length; i++) {
+    Console.WriteLine("Posizione" + i + ":" + cibiPreferiti[i]);
+}
+
+//prima posizione della classifica
+Console.WriteLine("Primo cibo:" + cibiPreferiti.First());
+
+//ultima posizione della classifica
+Console.WriteLine("Ultimo cibo:" + cibiPreferiti.Last());
