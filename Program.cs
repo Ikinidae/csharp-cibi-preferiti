1c2099f815ee0af5fd4b3bf1de484d53088af1bd
feder <federicaelia24@gmail.com>
2022-10-27 16:32:10 +0200

    snack 4 iniziato

--- Program.cs
+++ Program.cs
@@ -1,25 +1,102 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-string[] cibiPreferiti = { "Pasta", "Patate", "Broccoli", "Polpette", "Pasta al pesto", "Spinaci" };
+//string[] cibiPreferiti = { "Pasta", "Patate", "Broccoli", "Polpette", "Pasta al pesto", "Spinaci" };
 
 //lunghezza array
-Console.WriteLine(cibiPreferiti.Length);
+//Console.WriteLine(cibiPreferiti.Length);
 
 //stampare l’intero array in ordine indicando la posizione in classifica
-for (int i = 0; i < cibiPreferiti.Length; i++) {
-    Console.WriteLine("Posizione" + i + ": " + cibiPreferiti[i]);
-}
+//for (int i = 0; i < cibiPreferiti.Length; i++) {
+//Console.WriteLine("Posizione" + i + ": " + cibiPreferiti[i]);
+//}
 
 //prima posizione della classifica
-Console.WriteLine("Primo cibo: " + cibiPreferiti.First());
+//Console.WriteLine("Primo cibo: " + cibiPreferiti.First());
 
 //ultima posizione della classifica
-Console.WriteLine("Ultimo cibo: " + cibiPreferiti.Last());
+//Console.WriteLine("Ultimo cibo: " + cibiPreferiti.Last());
 
 //Stampate a video anche il cibo di mezza classifica, cioè che si trova nella posizione mediana.
-//divido per due, esce un numero con la virgola, scelgo un intero a scendere
-//float y = cibiPreferiti.Length / 2;
+// Se il numero di elementi è pari stampare i due elementi centrali
+//var y = cibiPreferiti.Length / 2;
+//Console.WriteLine("Cibi mediani: " + cibiPreferiti[y - 1] + " e " + cibiPreferiti[y]);
+
+// Se il numero di elementi è dispari scelgo l'elemento centrale: divido per due, esce un numero con la virgola, Floor arrotonda al numero più piccolo
 //Console.WriteLine("Cibi mediani: " + Math.Floor(y));
 
-int y = cibiPreferiti.Length / 2;
-Console.WriteLine("Cibi mediani: " + cibiPreferiti[y - 1] + " e " + cibiPreferiti[y]);
+//if (cibiPreferiti.Length % 2 == 0)
+//{
+//    //pari
+//    int ySup = cibiPreferiti.Length / 2;
+//    int yInf = ySup - 1;
+//    Console.WriteLine("Cibo middle: " + cibiPreferiti[yInf] + ", " + cibiPreferiti[ySup]);
+//}
+//else
+//{
+//    //dispari
+//    int y = cibiPreferiti.Length / 2;
+//    Console.WriteLine("Cibo middle: " + cibiPreferiti[y]);
+//}
+
+
+
+//Istruzioni condizionali
+
+//Snack1.
+//L’utente inserisce due numeri in successione. Il software stampa il maggiore.
+
+//Console.WriteLine("Inserisci il primo numero");
+//int firstNum = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("il tuo primo numero è: " + firstNum);
+
+//Console.WriteLine("Inserisci il secondo numero");
+//int secondNum = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("il tuo secondo numero è: " + secondNum);
+
+//if (firstNum > secondNum)
+//{
+
+//    Console.WriteLine("il numero più grande è: " + firstNum);
+//}
+//else
+//{
+//    Console.WriteLine("il numero più grande è: " + secondNum);
+//}
+
+//Snack2.
+
+//L’utente inserisce due parole in successione.
+//Console.WriteLine("Inserisci la prima parola");
+//string firstWord = Console.ReadLine();
+//Console.WriteLine("la tua prima parola è: " + firstWord);
+
+//Console.WriteLine("Inserisci la seconda parola");
+//string secondWord = Console.ReadLine();
+//Console.WriteLine("la tua seconda parola è: " + secondWord);
+
+//Il software stampa prima la parola più corta, poi la parola più lunga.
+//if (firstWord.Length < secondWord.Length)
+//{
+
+//    Console.WriteLine("la parola più corta è: " + firstWord + ", la parola più lunga è: " + secondWord);
+//}
+//else
+//{
+//    Console.WriteLine("la parola più corta è: " + secondWord + ", la parola più lunga è: " + firstWord);
+//}
+
+// snack 3: Il software deve chiedere per 10 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
+
+//int y = 0;
+//for (int i = 0; i < 10; i++)
+//{
+//    Console.WriteLine("inserisci un numero");
+//    int x = Convert.ToInt32(Console.ReadLine());    
+//    y += x;
+
+//}
+
+//Console.WriteLine("La somma dei tuoi numeri è " + y);
+
+// snack 4: Calcola la somma e la media dei numeri da 2 a 10.
+
